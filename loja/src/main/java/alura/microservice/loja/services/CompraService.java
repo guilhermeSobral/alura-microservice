8a705d4970d668e228f325ae37db1f50f57bc31a
Guilherme Barros <guilherme.barros@arista.com.br>
2020-02-03 13:09:35 -0300

    Adicionando Feign Client

--- loja/src/main/java/alura/microservice/loja/services/CompraService.java
+++ loja/src/main/java/alura/microservice/loja/services/CompraService.java
@@ -1,11 +1,9 @@
 package alura.microservice.loja.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import alura.microservice.loja.client.FornecedorClient;
 import alura.microservice.loja.dto.CompraDTO;
 import alura.microservice.loja.dto.InfoFornecedorDTO;
 
@@ -13,17 +11,12 @@ import alura.microservice.loja.dto.InfoFornecedorDTO;
 public class CompraService {
 	
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 
 	public void realizaCompra(CompraDTO dto) {		
 		
-		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
-				"http://fornecedor/info/" + dto.getEndereco().getEstado(), 
-				HttpMethod.GET,
-				null,
-				InfoFornecedorDTO.class);
-		
-		System.out.println(exchange.getBody().getEndereco());
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(dto.getEndereco().getEstado());
+		System.out.println(info.getEndereco());
 	}
 	
 	
